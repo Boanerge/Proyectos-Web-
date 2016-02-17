e3905643c9544757533dbd406f9911da087a1484
Antony Carrizo <antonyprogramacion96@gmail.com>
2016-02-17 00:12:33 -0430

    Esta es una calculadora mu simple, mejor denominada en mi pais como la 'Bodeguera'.

---
+++ Calculadora v.3/script.js
@@ -0,0 +1,52 @@
+//Valor que hay escrito en la pantalla.
+var pantalla = document.getElementById("pantalla");
+var punto = '.';
+//Obtener el valor que hay en la pantalla para concatenarlo con el numero presinado.
+var obtenerNum = function(Num){
+	var x = document.getElementById("pantalla");	
+	var concatenarNumeros = x.value + Num;
+	toString(x.value = concatenarNumeros);
+}
+
+var valor_viejo = 0, signo;
+
+//Operaciones
+var suma = function(){
+	valor_viejo = parseFloat(pantalla.value);
+	pantalla.value = '';
+	signo = '+';
+}
+var resta = function(){
+	valor_viejo = parseFloat(pantalla.value);
+	pantalla.value = '';
+	signo = '-';
+}
+var multiplicar = function(){
+	valor_viejo = parseFloat(pantalla.value);
+	pantalla.value = '';
+	signo = '*';
+}
+var division = function(){
+	valor_viejo = parseFloat(pantalla.value);
+	pantalla.value = '';
+	signo = '/';
+}
+//Resultado
+var resultado = function(){
+	var valor_A, valor_B;
+	valor_A = valor_viejo;
+	valor_B = (parseFloat(pantalla.value));
+	
+	if(signo == '+'){
+		pantalla.value = valor_A + valor_B;
+	}
+	else if(signo == '-'){
+		pantalla.value = valor_A - valor_B;
+	}
+	else if(signo == '*'){
+		pantalla.value = valor_A * valor_B;
+	}
+	else if(signo == '/'){
+		pantalla.value = valor_A / valor_B;
+	}
+}
